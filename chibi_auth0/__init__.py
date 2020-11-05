61f7790ab7e392a2e28a2a2eae90bc421ada50ec
dem4ply <dem4ply@gmail.com>
2020-11-05 15:22:39 -0600

    init commit

---
+++ chibi_auth0/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from chibi_auth0.chibi_auth0 import Chibi_auth0
+
+
+__author__ = """dem4ply"""
+__email__ = 'dem4ply@gmail.com'
+__version__ = '0.0.1'
+__all__ = [ 'Chibi_auth0' ]
